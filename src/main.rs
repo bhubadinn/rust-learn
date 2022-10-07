a895e5422da58ba5c2deca5e94ce23058814009a
bhubadinn <phoobadin.wrk.2200@gmail.com>
2022-10-08 01:06:00 +0700

    learn hello world, basics variable mut concept

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // 0
+    println!("Hello, World!");
+
+    // 1
+    let mut guess = String::new();
+    println!("Guess the number!");
+    println!("Please input your guess below : ");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Falied to read line");
+    println!("You guessed {guess}");
+
+    // 2
+    let apple = 5;
+    println!("You have {apple} apples!");
 }
